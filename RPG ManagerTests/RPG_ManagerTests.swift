b36db51f562d5fe0bbea9dd10e40645c3c8e6fa3
Anjali Narang <s1328134@monmouth.edu>
2024-11-17 14:43:22 -0500

    Initial Commit

---
+++ RPG ManagerTests/RPG_ManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RPG_ManagerTests.swift
+//  RPG ManagerTests
+//
+//  Created by Anjali Narang  on 11/17/24.
+//
+
+import Testing
+@testable import RPG_Manager
+
+struct RPG_ManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
