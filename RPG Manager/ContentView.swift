76cfb0d287c80ce66fd24bfb574514f28f27ef44
Anjali Narang <s1328134@monmouth.edu>
2024-11-17 15:38:40 -0500

    SwiftUI test

---
+++ RPG Manager/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  RPG Manager
+//
+//  Created by Anjali Narang  on 11/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject private var viewModel = TestViewModel()
+
+    var body: some View {
+        
+        VStack {
+            
+            TextField("Enter text here", text: $viewModel.inputText, onCommit: {
+                viewModel.handleSubmit() // submit if they press enter key in text field
+            })
+            .padding()
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            Button("Print to Console") {
+                viewModel.handleSubmit() // submit if they press button
+            }
+            .padding()
+        }
+        
+        .padding()
+        
+        Button("Change Screen") {
+            // NAVIGATION
+        }
+        
+    }
+}
---
+++ RPG Manager/TestViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  TestViewModel.swift
+//  RPG Manager
+//
+//  Created by Anjali Narang  on 11/17/24.
+//
+
+import SwiftUI
+
+class TestViewModel: ObservableObject {
+    @Published var inputText: String = ""
+    
+    func handleSubmit() {
+        print(inputText)
+        inputText = ""
+    }
+    
+}
