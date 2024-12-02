dc6767753c3890880c4cb6945150c043629df185
Anjali Narang <s1328134@monmouth.edu>
2024-12-02 15:17:32 -0500

    undo

--- RPG Manager/ViewModels/CharacterBagViewModel.swift
+++ RPG Manager/ViewModels/CharacterBagViewModel.swift
@@ -79,9 +79,35 @@ class CharacterBagViewModel: ObservableObject {
             if (item.type == "passive") { // if item is passively impacting stats right now
                 // since passive stat changes were applied directly to character when item was added to bag,
                 // we now have to remove these stat changes
-                let updateCharacter = unapplyStatChanges(characterID: bagOwner.characterID, itemID: itemID)
+                
+                var updateCharacter = bagOwner
+
+                switch item.impactsWhat {
+                case "health":
+                    updateCharacter.stats.health -= item.impact
+                    print("item impacts health: \(item.impact)")
+                case "attack":
+                    updateCharacter.stats.attack -= item.impact
+                    print("item impacts attack: \(item.impact)")
+                case "defense":
+                    updateCharacter.stats.defense -= item.impact
+                    print("item impacts defense: \(item.impact)")
+                case "speed":
+                    updateCharacter.stats.speed -= item.impact
+                    print("item impacts speed: \(item.impact)")
+                case "agility":
+                    updateCharacter.stats.agility -= item.impact
+                    print("item impacts agility: \(item.impact)")
+                case "hp":
+                    updateCharacter.stats.hp -= item.impact
+                    print("item impacts hp: \(item.impact)")
+                default:
+                    print("unhandled impact type: \(item.impactsWhat)")
+                }
+                
                 characterModel.updateCharacter(character: updateCharacter)
                 self.stats = characterModel.getTruncatedStats(characterID: bagOwner.characterID)
+                
             }
 
         }
@@ -96,7 +122,31 @@ class CharacterBagViewModel: ObservableObject {
     func equipItem(characterID: String) {
         var updateCharacter = characterModel.getCharacter(for: characterID)
         updateCharacter?.heldItem = itemID
-        updateCharacter = applyStatChanges(characterID: updateCharacter!.characterID, itemID: itemID)
+        
+        let item = itemModel.getItem(for: itemID)
+        switch item?.impactsWhat {
+        case "health":
+            updateCharacter!.stats.health += item!.impact
+            print("item impacts health: \(item?.impact ?? 0)")
+        case "attack":
+            updateCharacter!.stats.attack += item!.impact
+            print("item impacts attack: \(item?.impact ?? 0)")
+        case "defense":
+            updateCharacter!.stats.defense += item!.impact
+            print("item impacts defense: \(item?.impact ?? 0)")
+        case "speed":
+            updateCharacter!.stats.speed += item!.impact
+            print("item impacts speed: \(item?.impact ?? 0)")
+        case "agility":
+            updateCharacter!.stats.agility += item!.impact
+            print("item impacts agility: \(item?.impact ?? 0)")
+        case "hp":
+            updateCharacter!.stats.hp += item!.impact
+            print("item impacts hp: \(item?.impact ?? 0)")
+        default:
+            print("unhandled impact type: \(item?.impactsWhat ?? "unknown")")
+        }
+        
         characterModel.updateCharacter(character: updateCharacter!)
         self.stats = characterModel.getTruncatedStats(characterID: bagOwner.characterID)
     }
@@ -104,7 +154,31 @@ class CharacterBagViewModel: ObservableObject {
     func unequipItem(characterID: String) {
         var updateCharacter = characterModel.getCharacter(for: characterID)
         updateCharacter?.heldItem = nil
-        updateCharacter = unapplyStatChanges(characterID: updateCharacter!.characterID, itemID: itemID)
+        
+        let item = itemModel.getItem(for: itemID)
+        switch item?.impactsWhat {
+        case "health":
+            updateCharacter!.stats.health -= item!.impact
+            print("item impacts health: \(item?.impact ?? 0)")
+        case "attack":
+            updateCharacter!.stats.attack -= item!.impact
+            print("item impacts attack: \(item?.impact ?? 0)")
+        case "defense":
+            updateCharacter!.stats.defense -= item!.impact
+            print("item impacts defense: \(item?.impact ?? 0)")
+        case "speed":
+            updateCharacter!.stats.speed -= item!.impact
+            print("item impacts speed: \(item?.impact ?? 0)")
+        case "agility":
+            updateCharacter!.stats.agility -= item!.impact
+            print("item impacts agility: \(item?.impact ?? 0)")
+        case "hp":
+            updateCharacter!.stats.hp -= item!.impact
+            print("item impacts hp: \(item?.impact ?? 0)")
+        default:
+            print("unhandled impact type: \(item?.impactsWhat ?? "unknown")")
+        }
+        
         characterModel.updateCharacter(character: updateCharacter!)
         self.stats = characterModel.getTruncatedStats(characterID: bagOwner.characterID)
     }
