0bc05bd5af16c9dffa0b1db007cd32823ab6fea2
Anjali Narang <s1328134@monmouth.edu>
2024-12-18 19:08:49 -0500

    trying to fix map view

--- RPG Manager/Views/Story Views/Map/MapView.swift
+++ RPG Manager/Views/Story Views/Map/MapView.swift
@@ -10,70 +10,49 @@ import SwiftUI
 struct MapView: View {
     
     @ObservedObject var viewModel: MapViewModel = MapViewModel()
-    // I need to change it so that story is referenced in view model I think... so that updates happen right away
     
     @State private var showInfo: Bool = false
     
     // move this stuff into view model
     @State private var mapImage: UIImage? = nil
     @State private var icons: [DraggableIcon] = []
-        
+    
     var body: some View {
         
         NavigationView {
             
             VStack {
                 
-                if let imgurl = viewModel.currentStory!.mapImageURL, let url = URL(string: imgurl) {
+                if let mapImage {
                     
                     ZStack {
                         
-                        MapImageView(url: url)
+                        Image(uiImage: mapImage)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(Color.gray.opacity(0.2))
                         
-                        
-                        //                        Image(uiImage: mapImage)
-                        //                            .resizable()
-                        //                            .scaledToFit()
-                        //                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        //                            .background(Color.gray.opacity(0.2))
-                        
-                        ForEach(viewModel.mapItems, id: \.iconID) { icon in
-                            if let iconUrl = URL(string: viewModel.getIcon(type: icon.type, id: icon.iconID)) {
-                                
-                                AsyncImage(url: iconUrl) { phase in
-                                    switch phase {
-                                    case .empty:
-                                        ProgressView()
-                                            .frame(width: 40, height: 40)
-                                    case .success(let image):
-                                        image
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(width: 40, height: 40)
-                                    case .failure:
-                                        Color.gray.frame(width: 40, height: 40)
-                                    @unknown default:
-                                        EmptyView()
-                                    }
-                                }
-                                .position(CGPoint(x: CGFloat(icon.coordinates.x), y: CGFloat(icon.coordinates.y)))
-                                // .gesture(
-                                //                                    DragGesture()
-                                //                                        .onChanged { value in
-                                //                                            if let index = viewModel.mapItems.firstIndex(where: { $0.id == icon.id }) {
-                                //                                                viewModel.mapItems[index].position = value.location
-                                //                                            }
-                                //                                        }
-                                //                                )
-                                
-                                
-                            } // if
+                        ForEach(icons) { icon in
+                            icon.view
+                                .position(icon.position)
+                                .gesture(
+                                    
+                                    DragGesture()
+                                        .onChanged { value in
+                                            if let index = icons.firstIndex(where: { $0.id == icon.id }) {
+                                                icons[index].position = value.location
+                                                // would update coordinates here.
+                                            }
+                                        } // gesture onchange
+                                    
+                                ) // gesture
                             
                         } // foreach
                         
-                    } //zstack
+                    } // zstack
                     
-                } // map there condition
+                } // map img already there condition
                 
                 else {
                     
@@ -92,16 +71,15 @@ struct MapView: View {
                                },
                 trailing:
                     HStack {
-//                        Button(action: {
-//                            let newIcon = DraggableIcon(view: AnyView(Circle().fill(Color.red).frame(width: 40, height: 40)), position: CGPoint(x: 100, y: 100))
-//                            icons.append(newIcon)
-//                            }, label: {
-//                            Image(systemName: "plus")
-//                            }
-//                        )
+                        Button(action: {
+                            let newIcon = DraggableIcon(view: AnyView(Circle().fill(Color.red).frame(width: 40, height: 40)), position: CGPoint(x: 100, y: 100))
+                            icons.append(newIcon)
+                            }, label: {
+                            Image(systemName: "plus")
+                            }
+                        )
                         MapImgPicker { mapImg in
-                            viewModel.updateStoryMapImg(image: mapImg)
-                            //mapImage = mapImg
+                           mapImage = mapImg
                        }
                 }
             )
@@ -119,7 +97,123 @@ struct MapView: View {
 
 struct DraggableIcon: Identifiable {
     let id = UUID()
-    //var view: AnyView
-    var imageURL: String
+    var view: AnyView
     var position: CGPoint
 }
+
+//struct MapView: View {
+//    
+//    @ObservedObject var viewModel: MapViewModel = MapViewModel()
+//    // I need to change it so that story is referenced in view model I think... so that updates happen right away
+//    
+//    @State private var showInfo: Bool = false
+//    
+//    // move this stuff into view model
+//    @State private var mapImage: UIImage? = nil
+//    @State private var icons: [DraggableIcon] = []
+//        
+//    var body: some View {
+//        
+//        NavigationView {
+//            
+//            VStack {
+//                
+//                if let imgurl = viewModel.currentStory!.mapImageURL, let url = URL(string: imgurl) {
+//                    
+//                    ZStack {
+//                        
+//                        MapImageView(url: url)
+//                        
+//                        
+//                        //                        Image(uiImage: mapImage)
+//                        //                            .resizable()
+//                        //                            .scaledToFit()
+//                        //                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                        //                            .background(Color.gray.opacity(0.2))
+//                        
+//                        ForEach(viewModel.mapItems, id: \.iconID) { icon in
+//                            if let iconUrl = URL(string: viewModel.getIcon(type: icon.type, id: icon.iconID)) {
+//                                
+//                                AsyncImage(url: iconUrl) { phase in
+//                                    switch phase {
+//                                    case .empty:
+//                                        ProgressView()
+//                                            .frame(width: 40, height: 40)
+//                                    case .success(let image):
+//                                        image
+//                                            .resizable()
+//                                            .scaledToFit()
+//                                            .frame(width: 40, height: 40)
+//                                    case .failure:
+//                                        Color.gray.frame(width: 40, height: 40)
+//                                    @unknown default:
+//                                        EmptyView()
+//                                    }
+//                                }
+//                                .position(CGPoint(x: CGFloat(icon.coordinates.x), y: CGFloat(icon.coordinates.y)))
+//                                // .gesture(
+//                                //                                    DragGesture()
+//                                //                                        .onChanged { value in
+//                                //                                            if let index = viewModel.mapItems.firstIndex(where: { $0.id == icon.id }) {
+//                                //                                                viewModel.mapItems[index].position = value.location
+//                                //                                            }
+//                                //                                        }
+//                                //                                )
+//                                
+//                                
+//                            } // if
+//                            
+//                        } // foreach
+//                        
+//                    } //zstack
+//                    
+//                } // map there condition
+//                
+//                else {
+//                    
+//                    Text("Please upload a map image")
+//                    
+//                }
+//                
+//            } // vstack
+//            .navigationBarItems(
+//                
+//                leading: Button(action: {
+//                    showInfo = true
+//                }
+//                               ) {
+//                                   Image(systemName: "info.circle")
+//                               },
+//                trailing:
+//                    HStack {
+////                        Button(action: {
+////                            let newIcon = DraggableIcon(view: AnyView(Circle().fill(Color.red).frame(width: 40, height: 40)), position: CGPoint(x: 100, y: 100))
+////                            icons.append(newIcon)
+////                            }, label: {
+////                            Image(systemName: "plus")
+////                            }
+////                        )
+//                        MapImgPicker { mapImg in
+//                            viewModel.updateStoryMapImg(image: mapImg)
+//                            //mapImage = mapImg
+//                       }
+//                }
+//            )
+//            .sheet(isPresented: $showInfo) {
+//                InfoView()
+//            }
+//            //.onAppear {
+//            //}
+//            
+//        } // nav view
+//        
+//    } // view
+//    
+//}
+//
+//struct DraggableIcon: Identifiable {
+//    let id = UUID()
+//    //var view: AnyView
+//    var imageURL: String
+//    var position: CGPoint
+//}
